e37a831abd3784381d1ee9647773f25a34f23930
mrcreel <michaelrcreel@gmail.com>
2017-12-04 12:18:36 -0600

    Finally finished. Ready to push to codePen

--- script.js
+++ script.js
@@ -1,26 +1,76 @@
-var displayOneString = "";
-var displayTwoString = "";
-var numCurrent = 0;
+$(document).ready(function() {
+  var display1String = "";
+  var display2String = "";
+  var numCurrent = 0;
+  var plusMinus = 1;
 
-function setDisplay(display, arg) {
+  function setDisplay(display, arg) {
     /* body... */
     document.getElementById(display).innerHTML = arg;
-}
+  }
 
-$("document").click(function(e) {
-    var elementClassName = e.target.className; // get the classname of the element clicked
-    // var elementValue = e.target.value;
-    // var elementId = e.target.id;
-    // var arrClassNames = elementClassName.split(" ");
-    //
-    // var clicked = arrClassNames[4];
-    // if (clicked === "key-number") {
-    //     displayOneString.concat(elementValue);
-    //     numCurrent = parseFloat(displayOneString);
-    //     setDisplay(display1, numCurrent);
-    //
-    // }
-    // if (elementId === "keyClear") {}
-    // if (elementId === "keyBack") {}
-    // if (clicked === "key-operation") {}
+  function concatString(elementValue) {
+    /* body... */
+    display1String = display1String.concat(elementValue);
+    // numCurrent = parseFloat(display1String) * plusMinus;
+  }
+
+  $(".key").click(function(e) {
+    var elementClass = e.target.className; // get the classname of the element clicked
+    var elementValue = e.target.value;
+    var elementId = e.target.id;
+    var arrClasses = elementClass.split(" ");
+    var clicked = arrClasses[1];
+
+    if (elementId === "keyPoint") {
+      if (display1String.indexOf(".") === -1) {
+        concatString(elementValue);
+        setDisplay("display1", display1String);
+      }
+    }
+
+    if (elementId === "keyClear") {
+      display1String = "";
+      display2String = "";
+      numCurrent = 0;
+      plusMinus = 1;
+      setDisplay("display1", 0);
+      setDisplay("display2", 0);
+    }
+
+    if (elementId === "keyBack") {
+      display1String = display1String.slice(0, -1);
+      setDisplay("display1", display1String);
+    }
+
+    if (clicked === "key-number") {
+      concatString(elementValue);
+      setDisplay("display1", display1String);
+    }
+
+    if (elementId === "keyNegative") {
+      if (display1String[0] !== "-") {
+        display1String = "-".concat(display1String);
+      } else {
+        display1String = display1String.slice(1);
+      }
+      setDisplay("display1", display1String);
+    }
+
+    if (clicked === "key-operation") {
+      if(display1String.length >0){
+        display2String = display2String + " " + display1String + " " + elementValue;
+        setDisplay("display2", display2String);
+        display1String = "";
+      }
+    }
+    if (elementId === "keyEqual") {
+        var answer = eval(display2String.slice(0, -2));
+        setDisplay("display2", display2String + " " + answer);
+        display2String = "";
+        setDisplay("display1", answer);
+    }
+
+  });
+
 });
